ea18483f7db075aaa5c7c3efda58a040887c5ad1
sammy8825 <sb25112001@gmail.com>
2022-09-26 19:31:16 +0530

    Added Pin and Delete Functionality

--- src/components/Note.js
+++ src/components/Note.js
@@ -6,13 +6,18 @@ export default function Notes(props) {
 
     function handleClick() {
         changeSave(!saved);
+        if (!saved) props.saveNote(props.id);
+    }
+
+    function handleDelete() {
+        props.deleteNote(props.id);
     }
     return (
-        <div className="sticky notes" style={{ border: "1px solid black", display: "flex", flexDirection: "column", padding: "1% 3%" }}>
-            <p style={{ textAlign: "end" }} onClick={handleClick}> {saved ? <FaBookmark /> : <FaRegBookmark />}</p>
+        <div className="sticky notes" id={props.id} style={{ border: "1px solid black", display: "flex", flexDirection: "column", padding: "1% 3%" }}>
+            <p style={{ textAlign: "end" }}> <button onClick={handleClick}> {saved ? <FaBookmark /> : <FaRegBookmark />} </button></p>
             <input type="text" placeholder="Enter your title here" style={{ fontSize: "24px" }} />
             <textarea placeholder="Enter your notes here" style={{ display: "block", marginTop: "15px", height: "25vh", border: "none" }} />
-            <button> <FaRegTimesCircle /></button>
+            <button onClick={handleDelete}> <FaRegTimesCircle /></button>
         </div>
     )
 }
--- src/components/Page.js
+++ src/components/Page.js
@@ -3,23 +3,49 @@ import Note from "./Note";
 import { Grid } from '@mui/material';
 import { FaPlus } from 'react-icons/fa';
 
+import { nanoid } from "nanoid";
+
 
 export default function App() {
-    const [note, addNote] = React.useState([<Grid item md={4} key={0}>
-        <Note />
-    </Grid>]);
+
+    const [notes, setNote] = React.useState([newNote()]);
+
+    function newNote() {
+        let id = nanoid();
+        return (
+            <Grid item md={4} key={id}>
+                <Note id={id} deleteNote={deleteNote} saveNote={saveNote} />
+            </Grid>
+        )
+    }
 
     function addNewNote() {
-        addNote(notes => [<Grid item md={4} key={note.length}>
-            <Note />
-        </Grid>, ...notes]);
+        setNote(note => [newNote(), ...note]);
+    }
+
+    function saveNote(id) {
+        setNote(note => {
+            let savedNote;
+            for (let i = 0; i < note.length; i++) {
+                console.log(note[i].key);
+                if (note[i].key === id)
+                    savedNote = note[i];
+            }
+            return [savedNote, ...note.filter(n => n.key !== id)];
+        });
+    }
+
+    function deleteNote(id) {
+        setNote(note => {
+            return note.filter(n => n.key !== id);
+        })
     }
 
     return (
         <>
             <button onClick={addNewNote}><FaPlus /></button>
             <Grid container spacing={2}>
-                {note}
+                {notes}
             </Grid>
         </>
     );
